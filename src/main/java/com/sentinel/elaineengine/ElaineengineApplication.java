57cca6d12f2c38d946233d2916e7bb8416e2849e
Armando Reyna <areyna@sentinel.com>
2020-04-08 08:49:27 -0500

    starter

---
+++ src/main/java/com/sentinel/elaineengine/ElaineengineApplication.java
@@ -0,0 +1,13 @@
+package com.sentinel.elaineengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElaineengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElaineengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sentinel/elaineengine/ElaineengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinel.elaineengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElaineengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
